cda657bb06e5269b82469fc814f8b55b1c7eb158
Niels <niels95debruyne@hotmail.com>
2018-05-21 16:00:18 +0200

    nothing special

--- src/org/necrotic/Configuration.java
+++ src/org/necrotic/Configuration.java
@@ -75,9 +75,9 @@ public class Configuration {
 	
 	public final static String SERVER_HOST() {
 		// return "world1.necrotic.org"; // your live server's IP
-
-		//return "25.17.162.241"; // matt local
-		return "25.28.57.167"; //niels local
+		//return "25.17.86.12"; // your local server's IP
+		//return "25.28.57.167";
+		return "25.17.86.12";
 	}
 
 }
